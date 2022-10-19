afe61d8f21ef76a447b2f5829f965c48261f05c1
Graham KIff <gkiff@yahoo.com>
2022-10-19 06:28:53 +0100

    Add project files.

---
+++ AM_Persistence/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter integer (0-99): ");
+int Value = Convert.ToInt16(Console.ReadLine());
+Console.Write("Calculate additive or multiplicative persistence (a or m)? ");
+string Operation = Console.ReadLine();
+int Count = 0;
+
+while(Value > 9)
+{
+    if(Operation == "a")
+    {
+        Value = (Value / 10) + (Value % 10);
+    }
+    else
+    {
+        Value = (Value / 10) * (Value % 10);
+    }
+    Count ++;
+}
+
+Console.Write("The persistence is: ");
+Console.WriteLine(Count);
